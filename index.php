d37e41f78d2cb012a9d5085dbccdc8166e7c55ea
rudi8848 <anna.v8848@gmail.com>
2019-11-09 17:22:49 +0200

    database configuration

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-
+//phpinfo();
 echo 'camagru';
+
+try {
+
+	require_once('config/setup.php');
+
+
+	$query = 'select version() as version';
+
+	$ver = $db->query($query);
+
+	$version = $ver->fetch();
+
+	echo $version['version'];
+}
+catch(Exception $e)
+{
+	echo $e->getMessage();
+}
