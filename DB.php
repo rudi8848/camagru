099653c17ddfd6cf390704e10dec3355f8fddbbc
rudi8848 <anna.v8848@gmail.com>
2019-11-23 20:19:57 +0200

    added db  class

---
+++ DB.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once(__DIR__.'/config/database.php');
+class DB
+{
+
+	public static function getConnection()
+	{
+		$host = getenv('DB_HOST');
+		$dbname = getenv('DB_NAME');
+		$user = getenv('DB_USER');
+		$password = getenv('DB_PASSWORD');
+
+		$dsn = "mysql:host=$host;dbname=$dbname";
+		$db = new PDO($dsn, $user, $password);
+
+		return $db;
+	}
+}
