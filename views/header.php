7bf01233fa56cc9de0406f4624feeda4df981d6a
rudi8848 <anna.v8848@gmail.com>
2019-12-15 21:08:46 +0200

    header

--- views/header.php
+++ views/header.php
@@ -7,23 +7,25 @@
         </head>
         <body>
             <header>
+                <img src="/views/styles/pic/fotic.png"  class="logo-img">
+                <span class="logo">Camagru</span>
               <nav class="main-menu">
 
-                  <a href="/gallery">Gallery</a>
+
                 <?php if (empty($_SESSION['user']['id'])) : ?>
-                  <a href="/login">Login</a>
-                  <a href="/signup">Sign Up</a>
+                    <a href="/login"><span class="menu-pic login-pic"></span>Login</a>
+                    <a href="/signup"><span class="menu-pic login-pic"></span>Sign Up</a>
                 <?php else : ?>
 
-                  <a href="/settings">Settings</a>
-                  <a href="/logout">Logout</a>
+                    <a href="/logout"><span class="menu-pic logout-pic"></span>Logout</a>
+                    <a href="/settings"><span class="menu-pic settings-pic"></span>Settings</a>
 
-                    <p class="username">Hello, <?=$_SESSION['user']['name']?></p>
                 <?php endif; ?>
-
+                  <a href="/gallery"><span class="menu-pic gallery-pic"></span>Gallery</a>
               </nav>
 
             </header>
             <?php if (!empty($_SESSION['user']['id'])) :?>
                 <br/><a id="btn-new" href="/new">Upload new photo</a><br/>
+                <p class="username">Hello, <?=$_SESSION['user']['name']?></p>
             <?php endif; ?>
